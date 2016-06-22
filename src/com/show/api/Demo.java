a8b0153b340f79043c01a49e181d1c3805c29da1
15807552923 <penel@qq.com>
2016-06-22 16:33:14 +0800

    更新

--- src/com/show/api/Demo.java
+++ src/com/show/api/Demo.java
@@ -6,7 +6,7 @@ import com.show.api.FileItem;
 import com.show.api.ShowApiRequest;
 
 public class Demo {
-
+//
 	public static void main(String adfas[]) throws  Exception{ 
 		   String res=new ShowApiRequest("http://route.showapi.com/6-1","23","187f6d7af5cd4207a83ce5c3a963af60")
 		   .addTextPara("num","15914867924")
